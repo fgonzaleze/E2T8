758345d7f1eb26101dcce39a7d7a16a94c2bfe5d
34611 <34611@AJARE>
2023-10-30 20:23:21 +0100

    Agregar archivos de proyecto.

---
+++ E2T8/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace E2T8
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ E2T8/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace E2T8
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
